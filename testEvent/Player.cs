23f93fe4493bc26b018ae34dd3a0ed050f12c702
Daniel <Danielmb@gmx.de>
2021-09-11 18:12:35 +0200

    Fertig

---
+++ testEvent/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testEvent
+{
+   public class Player
+    {
+        public Player(int _health)
+        {
+            Health = _health;
+            OnHealthChangedeventHandler += new EventHandler<EventArgs>(OnHealthChanged);
+        }
+
+        private int health;
+        public int Health 
+        {   get => health;
+            private set 
+            {
+                health = value;
+                OnHealthChanged(this, EventArgs.Empty);
+            } 
+        }
+
+        EventHandler<EventArgs> OnHealthChangedeventHandler;
+
+        public virtual void OnHealthChanged(object sender, EventArgs args)
+        {
+            Console.WriteLine("Player: Health " + sender.ToString() + " " + "wurde geändert" + "Health: " + Health);
+        }
+
+        public void ShowHealth()
+        {
+            Console.WriteLine("Player Health : " + Health);
+        }
+
+        public void SlapSelf(int damage)
+        {
+            Health -= damage;     
+        }
+
+    }
+}
--- testEvent/Program.cs
+++ testEvent/Program.cs
@@ -6,11 +6,55 @@ namespace testEvent
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Player player = null;
 
 
+                Console.WriteLine("Möchten sie einen neuen spieler erstellen?\n1 = Ja\n2 = Nein");
+                string eingabe = Console.ReadLine();
 
+                if (int.TryParse(eingabe, out int zahl))
+                {
+                    if(zahl == 1)
+                    {
+                        player = new Player(20);
+                        Console.WriteLine("Ein neuer spieler wurde erstellt");
+                    }
+                    if(zahl == 2)
+                    {
+                        Console.WriteLine("Es wurde kein neuer spieler erstellt spiel ende");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Die eingabe war nich richtig");
+                }
 
-            Console.ReadLine();
+                if(player != null)
+                {
+                    Console.WriteLine("Möchten sie den spieler hauen?\n1 = Ja\n2 = Nein");
+
+                    string eingabe1 = Console.ReadLine();
+
+                    if (int.TryParse(eingabe1, out int zahl2))
+                    {
+                        if (zahl2 == 1)
+                        {                   
+                            Console.WriteLine("Spieler wird gehaun");
+                            player.SlapSelf(5);
+                        }
+                        if (zahl2 == 2)
+                        {
+                            Console.WriteLine("Der spieler wurde verschont");
+                        }
+                    }
+                }
+               
+
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
     }
 }
